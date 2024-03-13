961f6a7af7eddd30ef25b3efaa14b9850f6b42e0
csainathreddy <162982760+csainathreddy@users.noreply.github.com>
2024-03-13 09:39:44 +0530

    Add files via upload

---
+++ pythonfile.2.py
@@ -0,0 +1,213 @@
+# print(a,b,c)
+ #----> swapping of variables
+ a =7
+ b = 5
+ # Eg:1
+ # temp=a #temp=7
+ #a=b #a=5
+ #b=temp #b=7
+ # # a=5,#b=7
+ #print(a,b)
+ #Eg:2
+ a=a+b #a=12
+ b=a-b #b=12-7=5
+ #a=a-b #a=12-5=7
+  #print(a,b)
+ #a, b=b, a# only in python
+ # print(a,b)
+  a=a*b #a=35
+  b=a/b #b=35/7 =5.0
+  a=a/b #a=35/5 7.0
+  print(int(a), int(b))
+  a = 7
+  b = 8
+  print(a))
+  print(id(b))
+   #------> keywords
+  #keywords are reserved word which provides special meaning to
+  #compiler or intertor
+   #print(keywords. kwlist)
+  # print(len(keyword.kwlist))
+   #print(type(keyword.kwlist))
+  # To check if the string is keyword or not
+  #print(keyword.iskeyword("sid"))# false
+  #if = 8
+  #print(if) #error coz if is a keyword
+  #!----> literals
+  #liter is the constant value assigned to a vaiable
+  #A Variable is consider to be constant when it is defines
+  #in caps
+  #a=78 # a is variable
+  #A=78 #Ais constant
+  11 =[6,7,8,9,0]
+  n1 =89+7j
+  print(hash(n1)
+    f1 = [7,8,9]
+ print(hash(f1))# error --> list is non-constant or mutable datatype and
+       #print (id(a))
+        #print(id(b))
+        #!----> operators
+        #?operation are symbols which is used to perfeorm vaioues
+        #arithmatic
+        #assignment
+        #logical
+        #reation or comparison
+        #bitwise
+        #Identity
+        #membership
+        # todo ----> aritmatic -->,+, -, *,///,**
+        #Eg:1
+        #a = 8
+        #b = 6
+        #c = 9
+        #! // --> floor devision
+        #   **--> used for power of a number
+        #! comparson--> ==,>,< !=, <=, > =
+        # a =8
+        # b =7
+        #print(a>b)# true
+        #a=9
+        #b=5
+        print(a&b)
+        #2^4 2^3 2^2 2^1 2^0
+        # 8 4 2 1
+        # 0 1 1 1 #--> 7
+        # 0 1 0 0 #--> 4 &
+        #-----------------------
+        # 0 1 0 0
+        # 0 1 0 0
+        # a = 9876
+        #print(~a)
+        #a = 9
+        # 128 64 32 16 8 4 2 1
+        # 0 0 0 1 0 0 1# -->  9
+        # 1 1 1 1 0 # --> -10
+        # 256 128 64 32 16 8 4 2 1
+        # 0 0 0 0 0 0 0 1 0 1
+        # 0 0 0 0 0 0 1 0 0 0--> 40
+        # 107
+        #<<,>>
+        #print(5>>1)
+        # 16>4
+        # ! Logical --> used to commpar the expession
+        # and, or , not
+        a = 6
+        # print (a>3 and a<10)
+        #print (a>7 or a<30)
+        #print (not(a>
+        # ? are stored
+         a = 8
+        b = 8
+        #print (a is b
+        #a = [1, 2, 3]
+        #b =[1,2,]
+        #print (a is not b)
+        #! membership operator
+        # in, not in
+        11 =[7,8,9,0,6,5]
+        #num =55
+        #print(num in 11)
+        #print(num not in 11)
+        # num =678
+        # print(8 in num) # error
+        # if, elif ,else
+        #Eg:1
+        #--> C syntax
+        # staement
+        # statement
+        #  statement
+        #  statement
+        # statement
+        #eg:1
+        a=6
+        #print ("hey")
+        #eg:4
+        #a= none
+        #a=Falese
+        #a=""
+        #ifa:
+         print("yes")
+        # else:
+        print("no")
+        #a number is even or odd
+        n = int(input("enter the number:")
+                if n %2==0
+            #name,age,nationality
+                #18 above,indian
+                name = input("Enter the name:")
+                age = int(input("enter the age:")
+                nationliy = input ("enter the nation :")
+
+
+                if age>=18 and nationliy=="Indian":
+                          print("eligible for vote")
+                          else:
+                              print("not eligible")
+
+
+
+
+                
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+
+
+
+    
+    
+        
+        
+
+
+
+
+        
+
+    
+        
+   
+  
+
+  
+
+  
+
